5c8a1185cecdfb645f4d09c8d0a611f4f1a86d3b
jiaying.lu <lujiaying93@foxmail.com>
2015-07-11 17:43:16 +0800

    change some unimport things

--- wsgi.py
+++ wsgi.py
@@ -18,5 +18,5 @@ application = engine
 if __name__ == '__main__':
     # Be runnable locally.
     app.debug = True
-    server = simple_server.make_server('localhost', 3010, application)
+    server = simple_server.make_server('0.0.0.0', 13418, application)
     server.serve_forever()
